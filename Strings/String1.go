c9f9fc318fa3566f055e361e22c779b452e5f718
ebnyame <ebnyame001@st.ug.edu.gh>
2024-12-15 19:44:42 +0000

    converting strings to unicode

---
+++ Strings/String1.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	name := "Exodus"
+	kName := "맥저지"
+
+	fmt.Println(utf8.RuneCountInString(kName))
+	fmt.Println(utf8.RuneCountInString(name))
+}
