1c23e4f94f2698ae985b1fcbf4f4b58e0ab371bc
ebnyame <ebnyame001@st.ug.edu.gh>
2025-01-06 13:58:12 +0000

    registration and login using if statement

--- Exercise/IfStatement/PassMe.go
+++ Exercise/IfStatement/PassMe.go
@@ -1,13 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func PassMe(){
-	func create(){
-		var username, password string
-		fmt.Println("Enter your username:")
-		fmt.Scan(&username)
-		fmt.Println("Enter your password:")
-		fmt.Scan(&password)
+func passMe() {
+	var username string
+	var password string
+	var lusername string
+	var lpassword string
+
+	// registration
+
+	fmt.Println("Create a new Account...")
+	fmt.Print("Enter your username: ")
+	fmt.Scan(&username)
+
+	fmt.Print("Enter your password: ")
+	fmt.Scan(&password)
+
+	// login
+	fmt.Println("login your account")
+	fmt.Println("Enter your username: ")
+	fmt.Scan(&lusername)
+	fmt.Print("Enter your password: ")
+	fmt.Scan(&lpassword)
+
+	// compare password and username
+	if username == lusername && password == lpassword {
+		fmt.Printf("Welcome! %s You've sucessfully login \n", lusername)
+	} else {
+		fmt.Println("Invalid credentials!")
 	}
+
 }
