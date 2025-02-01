1c33e80f19be63891624009dedef6257fd41d94a
ebnyame <ebnyame001@st.ug.edu.gh>
2025-02-01 22:57:57 +0000

    booking app

--- BOOKING-APP/main.go
+++ BOOKING-APP/main.go
@@ -9,4 +9,30 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have a total of %v tickets and %v are still available.\n", totalTicket, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var bookings [50]string
+
+	var firstName string
+	fmt.Println("first name: ")
+	fmt.Scan(&firstName)
+
+	var lastName string
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	var userMail string
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&userMail)
+
+	var userTickets int
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = totalTicket - userTickets
+	bookings[0] = firstName + " " + lastName
+	fmt.Println(bookings)
+
+	fmt.Printf("%v remaining tickets for the %v \n", remainingTickets, conferenceName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, userMail)
 }
