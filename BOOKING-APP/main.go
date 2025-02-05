5fa806899f2bd51e58b9740f1356033ba80090cd
ebnyame <ebnyame001@st.ug.edu.gh>
2025-02-05 22:14:46 +0000

    fixed the remaining tickets error

--- BOOKING-APP/main.go
+++ BOOKING-APP/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	const totalTicket = 50
-	var remainingTickets = 50
+	var remainingTickets = totalTicket
 	var conferenceName = "Go Conference"
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have a total of %v tickets and %v are still available.\n", totalTicket, remainingTickets)
@@ -29,16 +32,36 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		var totalRemainingTickets int
-		remainingTickets = totalTicket - userTickets
+		if userTickets > totalTicket {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", totalTicket, userTickets)
+			continue
+		}
+
+		// var totalRemainingTickets int
+		remainingTickets = remainingTickets - userTickets
 		fmt.Println(remainingTickets)
-		totalRemainingTickets = remainingTickets - userTickets
+		// totalRemainingTickets = remainingTickets - userTickets
 		bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("%v remaining tickets for the %v \n", totalRemainingTickets, conferenceName)
+		fmt.Printf("%v remaining tickets for the %v \n", remainingTickets, conferenceName)
 
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, userMail)
 
 		fmt.Printf("This is all our bookings: %v \n", bookings)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Println(firstNames)
+
+		noTickets := remainingTickets == 0
+		if noTickets {
+			fmt.Println("Our conference is booked out! Come next year ")
+			break
+		}
+
 	}
+
 }
