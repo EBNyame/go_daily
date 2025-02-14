a4e6e8e0e4395eb5c0de2129106acf8152febed9
ebnyame <ebnyame001@st.ug.edu.gh>
2025-02-14 08:17:59 +0000

    💡 Exercise: Write Your First Go Program

---
+++ GoDailyWithChat/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Your name
+// Your favorite programming language
+// "I'm learning Go for backend development!"
+
+func main() {
+	fmt.Println("My name is Exodus Blessed Nyame")
+	fmt.Println("I'm learning Go for backend development!")
+}
